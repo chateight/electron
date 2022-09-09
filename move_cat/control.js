c945cb5959ff44faee4119fcd3fff19c1ab63f04
rusami <isehara.jp@coderdojo.com>
2022-09-09 17:45:14 +0900

    # to make the code shorten

--- move_cat/control.js
+++ move_cat/control.js
@@ -1,11 +1,11 @@
 // constnt & variables
 const xmin = 0, ymin = 0;       // set moving area     
 const xmax = 800, ymax = 350;                
-let x = 1, y = 1;               // position integer
-let x_px = "", y_px = "";       // position string "px"
+let x = 1, y = 1;               // current position(integer) of the sprite
 let dir_x = 1, dir_y = 1;       // moving direction(1 or -1)
 let int_id;         // setInterval ID
 const step = 5;
+const pos = document.querySelector(".img1");    // "image" selector
 //
 // interval timer start function
 const tmr = () => {
@@ -13,27 +13,16 @@ const tmr = () => {
     start.removeEventListener("click", tmr);        // stop the listener
 }
 //
-// move function to control the cat location
-function move(){
-    const pos = document.querySelector(".img1");
-    x_px = x.toString() + "px";                   
-    pos.style.left = x_px;
-    y_px = y.toString() + "px";
-    pos.style.top = y_px;
+// move function to control the cat position
+function move(){                      
+    pos.style.left = x.toString() + "px";
+    pos.style.top = y.toString() + "px";
     //         
     (dir_x === 1) ? x += step: x -= step;
-        if(x >= xmax) {
-            dir_x = -1;
-        }else if(x <= xmin) {
-            dir_x = 1;
-        }
+    (x >= xmax) ? dir_x = -1:(x <= xmin) ? dir_x = 1:{};               
     //
     (dir_y === -1) ? y += step: y -= step;
-        if(y >= ymax) {
-            dir_y = 1;
-        }else if(y <= ymin) {
-            dir_y = -1;
-        }
+    (y >= ymax) ? dir_y = 1:(y <= ymin) ? dir_y = -1:{};        
     pos.style.transform = "scale(" + dir_x + "," + dir_y + ")";
 }
 //
